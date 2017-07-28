3cabb082f84ab6b5eea9e580fabdefce2517c502
SNandalan <snandal2@my.centennialcollege.ca>
2017-07-28 00:27:09 -0400

    Added the IHabitable interface.

---
+++ COMP123_S2017_Assignment4/IHabitable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Sushmita Nandalan
+ * Student no: 300923159
+ * Date: July 28th 2017
+ * Description: This is the IHabitable interface that is implemented by the TerrestrialPlanet class.
+ * Version: 0.1 - IHabitable interface created. 
+ */
+namespace COMP123_S2017_Assignment4
+{
+	/// <summary>
+	/// This is the IHabitable interface.
+	/// </summary>
+	public interface IHabitable
+	{
+		/// <summary>
+		/// This is the Habitable method.
+		/// </summary>
+		/// <returns>It returns a bool data type</returns>
+		bool Habitable();
+	}
+}
