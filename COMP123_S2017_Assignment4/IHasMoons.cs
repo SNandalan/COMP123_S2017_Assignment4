667620f16457178766cedb24fa7aa52169e2fb7a
SNandalan <snandal2@my.centennialcollege.ca>
2017-07-28 00:22:52 -0400

    Added the IHasMoons and IHasRings interfaces.

---
+++ COMP123_S2017_Assignment4/IHasMoons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Sushmita Nandalan
+ * Student no: 300923159
+ * Date: July 28th 2017
+ * Description: This is the IHasMoons interface that is implemented by the GiantPlant class and TerrestrialPlanet class.
+ * Version: 0.1 - IHasMoons interface created. 
+ */
+namespace COMP123_S2017_Assignment4
+{
+	/// <summary>
+	/// This is the IHasMoons interface.
+	/// </summary>
+	public interface IHasMoons
+	{
+		/// <summary>
+		/// This is the HasMoons method.
+		/// </summary>
+		/// <returns>It returns a bool data type</returns>
+		bool HasMoons();
+	}
+}
---
+++ COMP123_S2017_Assignment4/IHasRings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Sushmita Nandalan
+ * Student no: 300923159
+ * Date: July 28th 2017
+ * Description: This is the IHasRings interface that is implemented by the GiantPlant class.
+ * Version: 0.1 - IHasRings interface created. 
+ */
+namespace COMP123_S2017_Assignment4
+{
+	/// <summary>
+	/// This is the IHasRings interface.
+	/// </summary>
+	public interface IHasRings
+	{
+		/// <summary>
+		/// This is the HasRings method.
+		/// </summary>
+		/// <returns>It returns a bool data type</returns>
+		bool HasRings();
+	}
+}
